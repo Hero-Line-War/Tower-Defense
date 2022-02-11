d438172c3b7a70fca70e27eab8e01da059392ea6
Aquemz <augustinbourgis@gmail.com>
2022-02-11 15:51:51 +0100

    Generation map + modif camera + sprite

--- Assets/Scenes/Script/LevelManager.cs
+++ Assets/Scenes/Script/LevelManager.cs
@@ -1,14 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject tile;
+    [SerializeField]
+    public GameObject[] tiles;
+    private Camera mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = Camera.main;
+        mainCamera.transform.position = new Vector3(2.5f, 2.5f, -10);
+        mainCamera.orthographicSize = 10f;
         CreateTileOnMap();
     }
 
@@ -20,15 +26,27 @@ public class LevelManager : MonoBehaviour
 
     private void CreateTileOnMap()
     {
-        float tileSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
-
-        for (int y = 0; y < 5; y++)
+        string[] mapData = new string[]
         {
-            for (int x = 0; x < 5; x++)
+            "1111", "1111", "2222", "3333", "0000"
+        };
+        int mapXSize = mapData[0].ToCharArray().Length;
+        int mapYSize = mapData.Length;
+
+        for (int y = 0; y < mapYSize; y++)
+        {
+            char[] newTiles = mapData[y].ToCharArray();
+            for (int x = 0; x < mapXSize; x++)
             {
-                GameObject newTile = Instantiate(tile);
-                newTile.transform.position = new Vector3(tileSize * x, tileSize * y, 1);
+                PlaceTile(newTiles[x].ToString(),x,y);
             }
         }
+    }
+
+    private void PlaceTile(string tileType, int x, int y)
+    {
+        int tileTypeIndex = Int32.Parse(tileType);
+        GameObject newTile = Instantiate(tiles[tileTypeIndex]);
+        newTile.transform.position = new Vector3(x, y, 1);
     }
 }
